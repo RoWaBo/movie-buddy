afcf5f24541f0dbe451517731133d30a7aedcf77
Robert <u1501851@rts-365.dk>
2022-03-01 10:57:22 +0100

    Installed react-hook-forms and created basic Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,63 @@
+import { AnimatePresence, motion } from "framer-motion";
+import ErrorMessage from "../components/ErrorMessage";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
 const Login = () => {
-	return <h1>Login</h1>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const [loginErrorMessage, setloginErrorMessage] = useState();
+
+	const onSubmit = userLogin => {
+		(async () => {
+			try {
+				console.log("userLogin: ", userLogin);
+			} catch (error) {
+				console.log(error);
+				setloginErrorMessage("User name or password is incorrect");
+			}
+		})();
+	};
+
+	return (
+		<AnimatePresence>
+			<motion.form onSubmit={handleSubmit(onSubmit)} layout>
+				<motion.input
+					layout
+					type="text"
+					placeholder={"username"}
+					onFocus={() => setloginErrorMessage(false)}
+					whileFocus={{ scale: 1.02 }}
+					{...register("username", {
+						required: "Write valid username",
+					})}
+				/>
+				<motion.input
+					layout
+					type="password"
+					placeholder="password"
+					onFocus={() => setloginErrorMessage(false)}
+					whileFocus={{ scale: 1.02 }}
+					{...register("password", {
+						required: "Write valid password",
+					})}
+				/>
+				{(errors.username || errors.password || loginErrorMessage) && (
+					<ErrorMessage icon>
+						{loginErrorMessage
+							? loginErrorMessage
+							: "Username and password is required"}
+					</ErrorMessage>
+				)}
+				<motion.button layout type="submit">
+					Login
+				</motion.button>
+			</motion.form>
+		</AnimatePresence>
+	);
 };
 
 export default Login;
