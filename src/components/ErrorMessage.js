9a7df8a9586350b685ec8ab3aaa02e1f1a893be3
Robert <u1501851@rts-365.dk>
2022-03-01 10:57:35 +0100

    Created ErrorMessage

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,44 @@
+import { css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import { BsExclamationCircle } from "react-icons/bs";
+import { motion } from "framer-motion";
+
+const ErrorMessage = ({ children, icon, ...props }) => {
+	const messageStyle = css`
+		font-size: 1rem;
+		color: white;
+		${!icon && "text-align: center;"};
+	`;
+	const containerStyle = css`
+		background: rgb(239 68 68 / 60%);
+		border-radius: 10px;
+		padding: 0.5rem 1rem;
+		${icon &&
+		`
+            display: flex;
+            place-items: center;      
+        `}
+	`;
+	const iconStyle = css`
+		width: 30px;
+		height: 30px;
+		margin-right: 1rem;
+		color: white;
+	`;
+
+	return (
+		<motion.div
+			css={containerStyle}
+			{...props}
+			key={"errorMessage"}
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: 20 }}
+			transition={{ duration: 0.5 }}>
+			{icon && <BsExclamationCircle css={iconStyle} />}
+			<p css={messageStyle}>{children}</p>
+		</motion.div>
+	);
+};
+
+export default ErrorMessage;
