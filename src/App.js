a536eb0395effcd267bed0f163b934ba28fdb631
Robert <u1501851@rts-365.dk>
2022-03-02 13:02:51 +0100

    Profile page created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<SignUp />} />
+				<Route path="/profile" element={<Profile />} />
 			</Routes>
 		</AuthProvider>
 	);
