a50055e99d41f85a47aa49760a7009deff681668
Robert <u1501851@rts-365.dk>
2022-03-05 13:03:17 +0100

    displayName update in addCurrentUserProfile and isHandleAvailable

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -1,17 +1,28 @@
-import { doc, getDoc, setDoc } from 'firebase/firestore'
+import {
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	collection,
+	query,
+	where,
+} from 'firebase/firestore'
 import { useAuth } from '../contexts/AuthContext'
 import { db } from '../firebaseConfig'
 
 const useProfile = () => {
-	const { currentUser } = useAuth()
+	const { currentUser, updateCurrentUser } = useAuth()
 	const profileRef = doc(db, `profiles/${currentUser.uid}`)
 
 	const addCurrentUserProfile = async (profileData) => {
-		return await setDoc(
+		await setDoc(
 			profileRef,
 			{ ...profileData, uid: currentUser.uid },
 			{ merge: true }
 		)
+		await updateCurrentUser({
+			displayName: profileData.handle,
+		})
 	}
 
 	const getCurrentUserProfile = async () => {
@@ -26,7 +37,21 @@ const useProfile = () => {
 		return movieGenres.data()
 	}
 
-	return { addCurrentUserProfile, getCurrentUserProfile, getMovieGenres }
+	const isHandleAvailable = async (handle) => {
+		const profilesRef = collection(db, 'profiles')
+		const q = query(profilesRef, where('handle', '==', handle))
+		const response = await getDocs(q)
+		let results = []
+		response.forEach((result) => results.push(result.data()))
+		return results.length > 0 ? false : true
+	}
+
+	return {
+		addCurrentUserProfile,
+		getCurrentUserProfile,
+		getMovieGenres,
+		isHandleAvailable,
+	}
 }
 
 export default useProfile
