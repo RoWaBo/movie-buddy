308e639e0589b80876256829548aae90896e8b24
Robert <u1501851@rts-365.dk>
2022-03-07 13:55:07 +0100

    added firebase storage to config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,5 @@ const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore()
 export const auth = getAuth(app)
+export const storage = getStorage(app)
 export default app
