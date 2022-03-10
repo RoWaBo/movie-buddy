64652ad542883cefeba8241c240c8a376706d64a
Robert <u1501851@rts-365.dk>
2022-03-10 11:36:49 +0100

    Added Search page with username search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,49 +1,102 @@
-import { useState } from 'react'
-import CenterContainer from '../components/CenterContainer'
+import { useState, useEffect } from 'react'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import SelectorMenu from '../components/SelectorMenu'
+import useSearch from '../hooks/useSearch'
+import useProfile from '../hooks/useProfile'
+import ErrorMessage from '../components/ErrorMessage'
+import ProfileListItem from '../components/ProfileListItem'
 
 const Search = () => {
+	const { getAllHandles } = useProfile()
+	const { searchByHandle } = useSearch()
 	const [searchInputValue, setSearchInputValue] = useState('')
 	const [selectedOption, setSelectedOption] = useState()
+	const [searchResult, setSearchResult] = useState([])
+	const [allHandles, setAllHandles] = useState([])
+	const [error, setError] = useState()
 
+	useEffect(() => {
+		if (allHandles.length > 0) return
+		;(async () => {
+			try {
+				if (selectedOption === 'username') {
+					const allHandles = await getAllHandles()
+					setAllHandles([...allHandles])
+				}
+			} catch (err) {
+				setError(err.message)
+			}
+		})()
+	}, [selectedOption, getAllHandles, allHandles])
+
+	useEffect(() => {
+		if (searchInputValue === '') return
+		const filteredHandles = allHandles.filter(({ handle }) => {
+			return handle.toLowerCase().includes(searchInputValue.toLowerCase())
+		})
+		setSearchResult([...filteredHandles])
+	}, [searchInputValue, allHandles])
+
+	const handleSubmit = async () => {
+		if (searchInputValue === '') return
+
+		console.log('Search: ', searchInputValue)
+		console.log('Selected Option: ', selectedOption)
+
+		let result
+
+		if (selectedOption === 'username') {
+			result = await searchByHandle(searchInputValue)
+			console.log(result)
+		}
+		setSearchResult([...result])
+	}
+
+	// === STYLING ===
 	const containerStyle = css`
 		.topHeading {
-			margin-bottom: 2rem;
+			margin: 2rem 0;
 		}
-		.input {
+		.input,
+		button {
 			display: block;
+			padding: 0.5rem;
+		}
+		.selectorMenu {
+			margin: 1rem 0;
 		}
 	`
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		console.log('Search: ', searchInputValue)
-		console.log('Selected Option: ', selectedOption)
-	}
-
 	return (
-		<CenterContainer css={containerStyle}>
+		<main css={containerStyle}>
 			<h1 className='topHeading'>User Search</h1>
-			<h2 className='subHeading'>Search options</h2>
+			<input
+				placeholder='search'
+				className='input'
+				type='text'
+				value={searchInputValue}
+				onChange={(e) => setSearchInputValue(e.target.value)}
+			/>
 			<SelectorMenu
+				className='selectorMenu'
 				options={['Genre', 'Username', 'Keywords']}
 				setSelectedOption={setSelectedOption}
 			/>
-			<form onSubmit={handleSubmit}>
-				<label className='label'>
-					Search
-					<input
-						className='input'
-						type='text'
-						value={searchInputValue}
-						onChange={(e) => setSearchInputValue(e.target.value)}
-					/>
-				</label>
-				<button type='submit'>Search</button>
-			</form>
-		</CenterContainer>
+			{/* <button onClick={handleSubmit}>Search</button> */}
+			<ul>
+				{searchInputValue !== '' &&
+					selectedOption === 'username' &&
+					searchResult.map(({ uid, handle, pictureURL }) => (
+						<ProfileListItem
+							key={uid}
+							handle={handle}
+							pictureURL={pictureURL}
+						/>
+					))}
+			</ul>
+			{error && <ErrorMessage>{error}</ErrorMessage>}
+		</main>
 	)
 }
 
